98c56daf5c7022183d34cc5075e9aedbb344b13c
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-05-30 09:23:42 +0200

    more restricted demo

---
+++ torch-refresher/demos/acceleration_v2.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch
+import timeit
+
+def main():
+    M = torch.rand((2000,2000))
+    M_time = timeit.timeit(lambda: M @ M.T, number=100)
+    print(f"[CPU] Time for 100 matrix multiplications: {M_time:.4f} seconds")
+
+    if torch.backends.mps.is_available():
+        M_mps = torch.rand((2000,2000), device="mps")
+        M_mps_time = timeit.timeit(lambda: M_mps @ M_mps.T, number=100)
+        print(f"[mps GPU] Time for 100 matrix multiplications: {M_mps_time:.4f} seconds")
+
+if __name__ == "__main__":
+    main()
