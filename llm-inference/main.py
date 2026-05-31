9ee0284d8b4b73f14ed7289d33301f354e4611ef
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-05-31 10:14:10 +0200

    vllm basic

--- llm-inference/main.py
+++ llm-inference/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from llm-inference!")
+from vllm import LLM, SamplingParams
 
+model_name = "Qwen/Qwen2.5-0.5B"
+
+def main():
+    llm = LLM(model=model_name, dtype="float16")
+    params = SamplingParams(temperature=0.7, top_p=0.9, max_output_tokens=512)
+
+    response = llm.generate("What is the capital of France?", sampling_params=params)
+    for r in response:
+        print(f"Generated text: {r.text}")
 
 if __name__ == "__main__":
     main()
