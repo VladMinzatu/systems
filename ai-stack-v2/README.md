92a589e2583f9884ddcaafafad9592091dab1c40
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-06-10 18:31:35 +0200

    agent using ollama

---
+++ ai-stack-v2/README.md
@@ -0,0 +1,6 @@
+## Local run
+
+Everything is configured in main.py for now, so just:
+```
+uv run main.py
+```
--- ai-stack-v2/main.py
+++ ai-stack-v2/main.py
@@ -1,5 +1,22 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.ollama import OllamaModel
+from pydantic_ai.providers.ollama import OllamaProvider
+
 def main():
-    print("Hello from ai-stack-v2!")
+    model = OllamaModel(
+        "qwen2.5:7b",
+        provider=OllamaProvider(
+        base_url="http://localhost:11434/v1"
+        ),
+    )
+
+    agent = Agent(  
+        model,
+        instructions='Be concise, reply with one sentence.',  
+    )
+
+    result = agent.run_sync('What is pydantic.ai?')  
+    print(result.output)
 
 if __name__ == "__main__":
     main()
