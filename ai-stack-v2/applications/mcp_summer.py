83a5a34e8de0115977d17313fd67b38ec7c4780d
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-06-13 12:32:31 +0200

    todo: typing and retries

--- ai-stack-v2/applications/mcp_summer.py
+++ ai-stack-v2/applications/mcp_summer.py
@@ -7,7 +7,7 @@ class AgenticSummer():
     def __init__(self, model):
         self.server = MCPServerStdio('uv', args=['run', './mcp-servers/sum-server.py', 'stdio'], timeout=10)
         self.agent = Agent(model, 
-        output_type=int,
+        output_type=int, # TODO: this fails half the time because the server returns a sentence - understand what pydantic does and where prompting support is needed + retry behavior.
         toolsets=[self.server])
 
     def run(self, query: str) -> int:
