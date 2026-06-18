d920b1d1cd4efb252ced166b7239795feb79a796
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-06-18 19:06:07 +0200

    indent

--- ai-stack-v2/applications/mcp_summer.py
+++ ai-stack-v2/applications/mcp_summer.py
@@ -7,8 +7,8 @@ class AgenticSummer():
     def __init__(self, model):
         self.server = MCPServerStdio('uv', args=['run', './mcp-servers/sum-server.py', 'stdio'], timeout=10)
         self.agent = Agent(model, 
-        output_type=int, # TODO: this fails half the time because the server returns a sentence - understand what pydantic does and where prompting support is needed + retry behavior.
-        toolsets=[self.server])
+            output_type=int, # TODO: this fails half the time because the server returns a sentence - understand what pydantic does and where prompting support is needed + retry behavior.
+            toolsets=[self.server])
 
     def run(self, query: str) -> int:
         return self.agent.run_sync(query)
