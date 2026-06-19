c10767fdb49018dbd6e84d3d01befae995d3fe60
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-06-19 20:00:53 +0200

    sum3

--- ai-stack-v2/main.py
+++ ai-stack-v2/main.py
@@ -4,8 +4,8 @@ from applications.mcp_summer import AgenticSummer
 def main():
     ollama_model = set_up_local_model()
     summer_agent = AgenticSummer(ollama_model)
-    result = summer_agent.run("What is 5 + 7?")
-    print(f"Result of 5 + 7: {result}")
+    result = summer_agent.run("What is 5 + 7 + 3?")
+    print(f"Result of 5 + 7 + 3: {result}")
 
 if __name__ == "__main__":
     main()
