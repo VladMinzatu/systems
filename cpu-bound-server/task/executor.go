80c15b30f5d5d083cd08bac50a35410ad529db8a
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-06-29 19:22:11 +0200

    add semaphore executor

--- cpu-bound-server/task/executor.go
+++ cpu-bound-server/task/executor.go
@@ -1,5 +1,6 @@
 package task
 
+// Simply executes the task synchronously in the current goroutine
 type SyncExecutor struct{}
 
 func NewSyncExecutor() *SyncExecutor {
@@ -7,5 +8,25 @@ func NewSyncExecutor() *SyncExecutor {
 }
 
 func (s *SyncExecutor) Execute(task Task) {
+	task.Run()
+}
+
+// Executes the task synchronously in the current goroutine
+// with a configurable level of parallelism.
+// When saturated, pending tasks block
+type SemaphoreExecutor struct {
+	sem chan struct{}
+}
+
+func NewSemaphoreExecutor(n int) *SemaphoreExecutor {
+	return &SemaphoreExecutor{
+		sem: make(chan struct{}, n),
+	}
+}
+
+func (e *SemaphoreExecutor) Execute(task Task) {
+	e.sem <- struct{}{}
+	defer func() { <-e.sem }()
+
 	task.Run()
 }
