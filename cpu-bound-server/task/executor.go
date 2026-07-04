73793d2486e7757be6ebf17375e96d0dafdba930
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-07-04 11:54:35 +0200

    rename

--- cpu-bound-server/task/executor.go
+++ cpu-bound-server/task/executor.go
@@ -34,12 +34,12 @@ func (e *SemaphoreExecutor) Execute(task Task) {
 // Executes the tasks in a worker pool with a configurable number of workers and queue size.
 // Similar to the SemaphoreExecutor, except the tasks are executed in dedicated goroutines instead of the caller's goroutine.
 type WorkerPoolExecutor struct {
-	jobs chan Task
+	tasks chan Task
 }
 
 func NewWorkerPoolExecutor(workers, queueSize int) *WorkerPoolExecutor {
 	e := &WorkerPoolExecutor{
-		jobs: make(chan Task, queueSize),
+		tasks: make(chan Task, queueSize),
 	}
 
 	for i := 0; i < workers; i++ {
@@ -50,11 +50,11 @@ func NewWorkerPoolExecutor(workers, queueSize int) *WorkerPoolExecutor {
 }
 
 func (e *WorkerPoolExecutor) worker() {
-	for task := range e.jobs {
+	for task := range e.tasks {
 		task.Run()
 	}
 }
 
 func (e *WorkerPoolExecutor) Execute(task Task) {
-	e.jobs <- task
+	e.tasks <- task
 }
