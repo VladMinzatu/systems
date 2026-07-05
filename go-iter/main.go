f6d28bb70fc5fc35ef7ac459c9f3abd652a430cd
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-07-05 10:03:00 +0200

    explanation

--- go-iter/main.go
+++ go-iter/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func Numbers(n int) func(func(int) bool) {
+func Numbers(n int) func(func(int) bool) { // notice that this just returns a function, no execution happens when invoked
 	return func(yield func(int) bool) {
 		for i := 1; i <= n; i++ {
 			if !yield(i) {
@@ -16,4 +16,13 @@ func main() {
 	for n := range Numbers(5) {
 		fmt.Println(n)
 	}
+	// roughly translates to:
+	// iterator := Numbers(5)
+	// iterator(func(v int) bool {
+	// 	   n := v
+	//     fmt.Println(n)
+	//     return true
+	// })
+	// -> the loop body becomes the body of the callback function.
+	// -> break and return are converted by the compiler to return false in the iterator callback function.
 }
