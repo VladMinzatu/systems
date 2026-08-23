d506f63b151f411a19d7fd500801d167f0b25283
Vlad Minzatu <minzatu.vlad@gmail.com>
2026-08-23 11:28:31 +0200

    count until cancel

--- go-concurrency-patterns/08-context/main.go
+++ go-concurrency-patterns/08-context/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"time"
+)
 
 func main() {
-	fmt.Println("Go")
+	CountUntilCancel()
+}
+
+func CountUntilCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		cancel()
+	}()
+
+	count(ctx)
+}
+
+func count(ctx context.Context) {
+	for i := 0; ; i++ {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			println(i)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
